9f7566be036705145ff6b38a566681665233b12b
Kat <katyake@gmail.com>
2019-06-14 17:42:38 +0100

    minir changes

--- StarWarsChars/src/app/app.module.ts
+++ StarWarsChars/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
+import { CreateCharacterComponent } from './create-character/create-character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TabsComponent,
     ListComponent,
-    ItemComponent
+    ItemComponent,
+    CreateCharacterComponent
   ],
   imports: [
     BrowserModule, NgbModule
